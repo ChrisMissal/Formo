3cff6977043bc1ec8cfdb964eb8ecb224d839c2f
Joao Matos Silva <kappy@acydburne.com.pt>
2013-02-23 12:36:22 +0000

    Added custom sections Features Tests

---
+++ src/Formo.Tests/FormoTests_CustomSection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Formo.Tests
+{
+    [TestFixture]
+    public class When_forced_to_use_a_string_key_CustomSection : When_forced_to_use_a_string_key
+    {
+        public When_forced_to_use_a_string_key_CustomSection()
+            : base("customSection")
+        {
+
+        }
+    }
+
+    [TestFixture]
+    public class When_using_typed_configuration_values_CustomSection : When_using_typed_configuration_values
+    {
+        public When_using_typed_configuration_values_CustomSection()
+            : base("customSection")
+        {
+
+        }
+    }
+
+    [TestFixture]
+    public class When_key_is_in_configuration_file_CustomSection : When_key_is_in_configuration_file
+    {
+        public When_key_is_in_configuration_file_CustomSection()
+            : base("customSection")
+        {
+
+        }
+    }
+
+    [TestFixture]
+    public class When_key_isnt_in_configuration_file_CustomSection : When_key_isnt_in_configuration_file
+    {
+        public When_key_isnt_in_configuration_file_CustomSection()
+            : base("customSection")
+        {
+
+        }
+    }
+}
---
+++ src/Formo.Tests/FormoTests_Default.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Formo.Tests
+{
+    [TestFixture]
+    public class When_forced_to_use_a_string_key_Default : When_forced_to_use_a_string_key
+    {
+
+    }
+
+    [TestFixture]
+    public class When_using_typed_configuration_values_Default : When_using_typed_configuration_values
+    {
+
+    }
+
+    [TestFixture]
+    public class When_key_is_in_configuration_file_Default : When_key_is_in_configuration_file
+    {
+
+    }
+
+    [TestFixture]
+    public class When_key_isnt_in_configuration_file_Default : When_key_isnt_in_configuration_file
+    {
+
+    }
+}
