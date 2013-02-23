b0878309b9789b724f225019b03623084ca0a2a6
Chris Missal <chris.missal@gmail.com>
2013-02-23 00:36:41 -0600

    wip on CloudConfiguration in reference to issue #5

---
+++ src/Formo.Cloud/CloudConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.WindowsAzure;
+
+namespace Formo.Cloud
+{
+    public class CloudConfiguration : Configuration
+    {
+        protected override string GetValue(string name)
+        {
+            return CloudConfigurationManager.GetSetting(name);
+        }
+    }
+}
