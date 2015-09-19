997bb647d97d08d6dc4a35219ab656fddbdb5377
Noel Bundick <nobun@microsoft.com>
2015-09-19 03:06:12 -0700

    Add support for Microsoft.WindowsAzure.Configuration 3.0.0.0 via reflection due to CloudConfigurationManager namespace change

--- src/Formo.Cloud/CloudConfiguration.cs
+++ src/Formo.Cloud/CloudConfiguration.cs
@@ -1,12 +1,22 @@
-﻿using Microsoft.WindowsAzure;
+﻿using System.Reflection;
 
 namespace Formo.Cloud
 {
     public class CloudConfiguration : Configuration
     {
+        private static readonly MethodInfo GetSettingMethod;
+
+        static CloudConfiguration()
+        {
+            var assembly = Assembly.Load("Microsoft.WindowsAzure.Configuration");
+            var type = assembly.GetType("Microsoft.Azure.CloudConfigurationManager")
+                ?? assembly.GetType("Microsoft.WindowsAzure.CloudConfigurationManager");
+            GetSettingMethod = type.GetMethod("GetSetting", BindingFlags.Static | BindingFlags.Public);
+        }
+
         protected override string GetValue(string name)
         {
-            return CloudConfigurationManager.GetSetting(name);
+            return (string) GetSettingMethod.Invoke(null, new object[] {name});
         }
     }
-}
+}}
